c3e49456ed982b74f0ecfdc6314f9331fcff0313
gaffneyd3 <36242672+gaffneyd3@users.noreply.github.com>
2018-09-21 19:12:02 -0400

    Add files via upload

--- Simple Blink/main.c
+++ Simple Blink/main.c
@@ -7,11 +7,12 @@
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	PM5CTL0 &= ~LOCKLPM5; //unlocks the gpio pins
-	P1DIR |= BIT0; //sets direction
+	P1SEL &= ~BIT0; //set select bits to be 0
+	P1SEL2 &= ~BIT0;
+	P1DIR |= BIT0; //set direction to be output
 	while(1){
-	    P1OUT ^= BIT0; //toggles led
-	    __delay_cycles(1000000); //delays so we can see the led blink
+	    P1OUT ^= BIT0; //toggle led from on to off
+	    __delay_cycles(1000000); //delay so that we can see the led blink
 	}
-	return (0);
+	return (0); //needs default return for method
 }
